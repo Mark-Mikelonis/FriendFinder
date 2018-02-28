13f6d5a506bee581c3d132d559a3480b764f640b
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-02-27 18:48:19 -0800

    fixed routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,9 @@
-var Friends = {
-   currLowestDiff: 100,
-   currBestMatch: null,
-   totalDifference: 0,
-   tempScore: 0,
+var currLowestDiff = 100;
+var currBestMatch;
+var totalDifference =0;
+var tempScore;
 
-findFriend: function (){
+function findFriend(){
     var totalDifference = 0;
     tempScore = friends[friends.length-1].scores;
     for (var i=0;i<friends.length;i++){
@@ -17,9 +16,9 @@ findFriend: function (){
        } 
     }
    
-   },
+}
 
-   compareScores: function (scores1, scores2){
+function compareScores(scores1, scores2){
     for(var i=0;i<10;i++){
         totalDifference += Math.abs(scores1[i] - scores2[i]);
         // console.log("totalDifference: " + totalDifference);
@@ -36,80 +35,75 @@ findFriend: function (){
         return false;
     }
     
-},
+}
 
 
 
-    friends: [
+    var friends = [
         {
             name:"Chris",
-            photo: "../data/images/chris.jpeg",
+            photo: "./images/chris.jpeg",
             scores: [5,1,4,4,5,1,2,5,4,1]
         },
         {
             name: "Jimmy",
-            photo: "../data/images/jimmy.jpeg",
+            photo: "./images/jimmy.jpeg",
             scores: [7,5,9,3,1,5,4,2,1,9]
         },
         {
             name: "Aria",
-            photo: "../data/images/aria.jpeg",
+            photo: "./images/aria.jpeg",
             scores: [5,2,8,9,4,6,1,2,3,4]
         },
         {
             name: "Bill",
-            photo: "../data/images/bill.jpeg",
+            photo: "./images/bill.jpeg",
             scores: [2,5,7,6,3,2,4,1,5,8]
         },
         {
             name:"Job",
-            photo: "../data/images/job.jpeg",
+            photo: "./images/job.jpeg",
             scores: [6,5,1,4,5,8,3,6,9,8]
         },
         {
             name: "Jill",
-            photo: "../data/images/jill.jpeg",
+            photo: "./images/jill.jpeg",
             scores: [4,5,8,2,3,6,8,7,4,5]
         },
         {
             name: "James",
-            photo: "../data/images/james.jpeg",
+            photo: "./images/james.jpeg",
             scores: [2,5,4,7,8,5,1,6,6,9]
         },
         {
             name: "Joanne",
-            photo: "../data/images/joanne.jpeg",
+            photo: "./images/joanne.jpeg",
             scores: [7,2,5,8,6,3,1,4,1,2]
         },
         {
             name: "Betty",
-            photo: "../data/images/betty.jpeg",
+            photo: "./images/betty.jpeg",
             scores: [5,2,4,7,9,5,1,2,3,5]
         },
         {
             name: "Lisa",
-            photo: "../data/images/lisa.jpeg",
+            photo: "./images/lisa.jpeg",
             scores: [1,2,5,2,4,6,5,1,2,8]
         },
         {
             name: "Donald",
-            photo: "images/donald.jpeg",
+            photo: "./images/donald.jpeg",
             scores: [5,8,1,2,3,6,9,7,2,3]
         },
         {
             name: "Meghan",
-            photo: "../data/images/meghan.jpeg",
+            photo: "./images/meghan.jpeg",
             scores: [4,5,3,4,5,7,2,3,4,6]
         }
     ]
-
-    
-}
-
-
 // findFriend();
 
-module.exports = Friends;
+module.exports = friends;
 
 
 
